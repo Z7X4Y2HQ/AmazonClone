681a885a4a40ed41e5f3b8bfb09dee14aaaf28ee
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2022-06-02 21:32:54 +0500

    nothin

--- src/Components/HomeProductList.js
+++ src/Components/HomeProductList.js
@@ -1,62 +1,68 @@
-import React, { useContext }from 'react'
-import { View, Image, Text, FlatList, Pressable, StyleSheet } from 'react-native'
+import React, { useContext } from "react";
+import { View, Image, Text, FlatList, Pressable, StyleSheet } from "react-native";
 import { NavBarContext } from "../Contexts/NavBarContext";
 
-
-
 export default HomeProductlist = ({ JSONdata, navigation }) => {
-
-  const {currentScreen, setCurrentScreen} = useContext(NavBarContext)
+  const { currentScreen, setCurrentScreen } = useContext(NavBarContext);
 
   return (
-    <FlatList data={JSONdata} renderItem={({ item }) => (
-      <View>
-        <Pressable onPress={() => { navigation.navigate('ProductPage', item); setCurrentScreen("ProductPageFromHome") }}>
-          <View style={styles.productContainer}>
-            <Image style={styles.productImage} source={{ uri: item.images[0] }} />
-            <View style={styles.productInfoContainer}>
-              <Text numberOfLines={3} style={styles.productTitle} >{item.description}</Text>
-              <View style={styles.priceContainer}>
-                <Text style={styles.superScriptText}>$</Text>
-                <Text style={{ fontSize: 24 }}>{item.newPrice}</Text>
-                <Text style={styles.superScriptText}>00</Text>
+    <FlatList
+      data={JSONdata}
+      renderItem={({ item }) => (
+        <View>
+          <Pressable
+            onPress={() => {
+              navigation.navigate("ProductPage", item);
+              setCurrentScreen("ProductPageFromHome");
+            }}
+          >
+            <View style={styles.productContainer}>
+              <Image style={styles.productImage} source={{ uri: item.images[0] }} />
+              <View style={styles.productInfoContainer}>
+                <Text numberOfLines={3} style={styles.productTitle}>
+                  {item.description}
+                </Text>
+                <View style={styles.priceContainer}>
+                  <Text style={styles.superScriptText}>$</Text>
+                  <Text style={{ fontSize: 24 }}>{item.newPrice}</Text>
+                  <Text style={styles.superScriptText}>00</Text>
+                </View>
               </View>
             </View>
-          </View>
-        </Pressable>
-      </View>
-    )}
+          </Pressable>
+        </View>
+      )}
     />
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   productContainer: {
     flex: 1,
-    flexDirection: 'row',
-    paddingBottom: 5
+    flexDirection: "row",
+    paddingBottom: 5,
   },
   productImage: {
     flex: 1,
     height: 100,
     width: 100,
-    resizeMode: 'contain',
-    margin: 5
+    resizeMode: "contain",
+    margin: 5,
   },
   productInfoContainer: {
     flex: 2,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
   productTitle: {
     margin: 5,
-    fontSize: 15
+    fontSize: 15,
   },
   priceContainer: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   superScriptText: {
     lineHeight: 28,
-    fontSize: 16
-  }
-})
+    fontSize: 16,
+  },
+});
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,64 +1,68 @@
-import { useContext, useEffect } from 'react';
-import { Text, BackHandler, Pressable, StyleSheet, View, Dimensions, Image, ScrollView } from 'react-native';
-import { SimpleLineIcons } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
+import { useContext, useEffect } from "react";
+import {
+  Text,
+  BackHandler,
+  Pressable,
+  StyleSheet,
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+} from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
-import Productsdata from '../assets/Data/FakeData.json'
-import random from '../assets/Data/random.json'
-import Laptops from '../assets/Data/Laptops.json'
-import Gaming from '../assets/Data/Gaming.json'
-import Mobile from '../assets/Data/Mobiles.json'
+import Productsdata from "../assets/Data/FakeData.json";
+import random from "../assets/Data/random.json";
+import Laptops from "../assets/Data/Laptops.json";
+import Gaming from "../assets/Data/Gaming.json";
+import Mobile from "../assets/Data/Mobiles.json";
 
-import BottomMenu from '../Components/BottomNavigationMenu'
-import TopSearchBar from '../Components/SearchBar';
-import HomeProductlist from '../Components/HomeProductList';
-import HomeProductGallery from '../Components/HomeProductGallery';
-import LocationIndicator from '../Components/LocationIndicator';
-import HomeHeadingText from '../Components/HomeHeadingText';
+import BottomMenu from "../Components/BottomNavigationMenu";
+import TopSearchBar from "../Components/SearchBar";
+import HomeProductlist from "../Components/HomeProductList";
+import HomeProductGallery from "../Components/HomeProductGallery";
+import LocationIndicator from "../Components/LocationIndicator";
+import HomeHeadingText from "../Components/HomeHeadingText";
 
-import { NavBarContext } from '../Contexts/NavBarContext';
+import { NavBarContext } from "../Contexts/NavBarContext";
 
 const { width } = Dimensions.get("window");
-const height = width * 101 / 115;
+const height = (width * 101) / 115;
 
 const headerImage = [
-  'https://m.media-amazon.com/images/I/51IpWF-q4RL._SR1236,1080_.jpg',
-  'https://m.media-amazon.com/images/I/51FkM+rv2BL._SR1236,1080_.jpg',
-  'https://m.media-amazon.com/images/I/81Q62pLRIzL._SR1236,1080_.png'
-]
+  "https://m.media-amazon.com/images/I/51IpWF-q4RL._SR1236,1080_.jpg",
+  "https://m.media-amazon.com/images/I/51FkM+rv2BL._SR1236,1080_.jpg",
+  "https://m.media-amazon.com/images/I/81Q62pLRIzL._SR1236,1080_.png",
+];
 
 const headerOver = [
-  'https://i.imgur.com/fPeEUIZ.png',
-  'https://m.media-amazon.com/images/I/51c0KsMQLYL._SR270,360_.png',
-  'https://m.media-amazon.com/images/I/51BwEB9glsL._SR270,360_.png',
-  'https://m.media-amazon.com/images/I/51c0KsMQLYL._SR270,360_.png',
-  'https://m.media-amazon.com/images/I/51BwEB9glsL._SR270,360_.png'
-]
+  "https://i.imgur.com/fPeEUIZ.png",
+  "https://m.media-amazon.com/images/I/51c0KsMQLYL._SR270,360_.png",
+  "https://m.media-amazon.com/images/I/51BwEB9glsL._SR270,360_.png",
+  "https://m.media-amazon.com/images/I/51c0KsMQLYL._SR270,360_.png",
+  "https://m.media-amazon.com/images/I/51BwEB9glsL._SR270,360_.png",
+];
 
 export default function HomeScreen({ navigation }) {
   const { currentScreen, setCurrentScreen } = useContext(NavBarContext);
-  const { previousScreens } = useContext(NavBarContext)
+  const { previousScreens } = useContext(NavBarContext);
 
-  console.log("\nPrevious Screeen : ", previousScreens)
-  console.log("\nCurrent Screeen : ", currentScreen)
-  console.log("\nPrevious Screen : ", previousScreens.current[previousScreens.current.length - 1])
+  console.log("\nPrevious Screeen : ", previousScreens);
+  console.log("\nCurrent Screeen : ", currentScreen);
+  console.log("\nPrevious Screen : ", previousScreens.current[previousScreens.current.length - 1]);
 
-  
   useEffect(() => {
     const backAction = () => {
-      setCurrentScreen(previousScreens.current[previousScreens.current.length - 1])
+      setCurrentScreen(previousScreens.current[previousScreens.current.length - 1]);
     };
 
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
-     
-    setCurrentScreen("Home")
+    const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
+
+    setCurrentScreen("Home");
 
     return () => backHandler.remove();
   }, []);
-
 
   return (
     <View style={styles.singleFlex}>
@@ -67,32 +71,30 @@ export default function HomeScreen({ navigation }) {
           <SearchBar navigation={navigation} />
           <ScrollView>
             <LocationIndicator />
-            <ScrollView style={styles.headerImageSlider} pagingEnabled showsHorizontalScrollIndicator={false} horizontal>
-              {
-                headerImage.map((image, index) => (
-                  <Image key={index} source={{ uri: image }} style={styles.headerImage} />
-                ))
-              }
+            <ScrollView
+              style={styles.headerImageSlider}
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              horizontal
+            >
+              {headerImage.map((image, index) => (
+                <Image key={index} source={{ uri: image }} style={styles.headerImage} />
+              ))}
             </ScrollView>
-            <ScrollView style={styles.headerCategorySlider} showsHorizontalScrollIndicator={false} horizontal>
-              {
-                headerOver.map((image, index) => (
-
-                  <Image
-                    key={index}
-                    source={{ uri: image }}
-                    style={styles.headerCategoryImage} />
-                ))
-              }
+            <ScrollView style={styles.headerCategorySlider} showsHorizontalScrollIndicator={false} horizontal
+            >
+              {headerOver.map((image, index) => (
+                <Image key={index} source={{ uri: image }} style={styles.headerCategoryImage} />
+              ))}
             </ScrollView>
-            <View style={{paddingTop: 75}} />
+            <View style={{ paddingTop: 75 }} />
             <View style={styles.backgroundColorWhite}>
               <HomeHeadingText text={"International top sellers"} />
               <HomeProductlist JSONdata={Productsdata} navigation={navigation} />
             </View>
             <View>
-              <Pressable onPress={() => navigation.navigate('List')}>
-                <Image style={styles.homeListImage} source={require('../assets/Images/img4.png')} />
+              <Pressable onPress={() => navigation.navigate("List")}>
+                <Image style={styles.homeListImage} source={require("../assets/Images/img4.png")} />
               </Pressable>
             </View>
             <View>
@@ -101,7 +103,7 @@ export default function HomeScreen({ navigation }) {
             </View>
             <Divider />
             <View style={styles.backgroundColorWhite}>
-            <HomeHeadingText text={"Gaming Accessories"} />
+              <HomeHeadingText text={"Gaming Accessories"} />
               <HomeProductlist JSONdata={Gaming} navigation={navigation} />
             </View>
             <Divider />
@@ -123,52 +125,52 @@ export default function HomeScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  singleFlex: { 
-    flex: 1 
+  singleFlex: {
+    flex: 1,
   },
-  imageThumbnail: { 
-    justifyContent: "center", 
-    alignItems: "center", 
-    height: 170, 
-    width: 170, 
-    resizeMode: "contain" 
+  imageThumbnail: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 170,
+    width: 170,
+    resizeMode: "contain",
   },
-  mainHomeContainer: { 
-    flex: 14, 
-    backgroundColor: "white", 
-    flexDirection: 'row', 
-    alignItems: 'flex-start' 
+  mainHomeContainer: {
+    flex: 14,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "flex-start",
   },
-  headerImageSlider: { 
-    elevation: 50, 
-    width 
+  headerImageSlider: {
+    elevation: 50,
+    width,
   },
-  headerImage: { 
-    width, 
-    height, 
-    resizeMode: 'contain' 
+  headerImage: {
+    width,
+    height,
+    resizeMode: "contain",
   },
   headerCategorySlider: {
-    elevation: 300, 
-    position: 'absolute', 
-    top: 280, 
-    width
+    elevation: 300,
+    position: "absolute",
+    top: 280,
+    width,
   },
   headerCategoryImage: {
-    height: width / 2, 
-    width: 130, 
-    margin: 8, 
-    borderRadius: 3, 
-    resizeMode: 'contain'
+    height: width / 2,
+    width: 130,
+    margin: 8,
+    borderRadius: 3,
+    resizeMode: "contain",
   },
   homeListImage: {
-    width, 
-    height: width / 2.5, 
-    borderWidth: 5, 
-    borderColor: '#d4dbdb', 
-    resizeMode: 'contain'
+    width,
+    height: width / 2.5,
+    borderWidth: 5,
+    borderColor: "#d4dbdb",
+    resizeMode: "contain",
   },
   backgroundColorWhite: {
-    backgroundColor: 'white'
-  }
+    backgroundColor: "white",
+  },
 });
