0b59d759897370eccc61cf5cdd3dd8b5dd160292
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2022-06-14 20:46:03 +0500

    Create YourOrder.js

---
+++ src/pages/YourOrder.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default function YourOrder() {
+
+  return(
+    <>
+    <View style = {styles.container}>
+
+    </View>
+    </>
+  )
+}
+
+let style = StyleSheet.create(
+  {
+    container:{
+      flex: 1,
+      backgroundColor: 'aqua'
+    }
+  }
+)
